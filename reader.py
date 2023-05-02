e0d0529aa99f54a7fe9254db30899f694d5f3a41
Nanashi <alksndr.zln@gmail.com>
2023-05-02 17:30:06 +0300

    Added completely functional writer.py with hash

--- reader.py
+++ reader.py
@@ -6,13 +6,13 @@ import argparse
 
 async def main(args):
 
-    host = args.host[0]
-    port = int(args.port[0])
+    host = args.host
+    port = args.port
     history = args.history
 
     reader, writer = await asyncio.open_connection(host=host, port=port)
     if history:
-        async with aiofiles.open('data.txt', 'a', encoding='utf-8') as file:
+        async with aiofiles.open(args.history, 'a', encoding='utf-8') as file:
             await file.write(f'[{datetime.now().strftime("%d.%m.%y %H:%M")}] Установлено соединение\n')
     try:
         while True:
@@ -20,7 +20,7 @@ async def main(args):
             data_decoded = data.decode()
             print(data_decoded)
             if history:
-                async with aiofiles.open('data.txt', 'a', encoding='utf-8') as file:
+                async with aiofiles.open(args.history, 'a', encoding='utf-8') as file:
                     await file.write(f'[{datetime.now().strftime("%d.%m.%y %H:%M")}] {data_decoded}')
     except UnicodeDecodeError:
         writer.close()
@@ -29,9 +29,9 @@ async def main(args):
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('--host', action='append', help='указать хост')
-    parser.add_argument('--port', action='append', help='указать порт')
-    parser.add_argument('--history', action='store_true', help='включить/выключить запись переписки')
+    parser.add_argument('--host', help='указать хост')
+    parser.add_argument('--port', type=int, help='указать порт')
+    parser.add_argument('--history', help='указать будет ли запись переписки и куда')
     args = parser.parse_args()
 
     asyncio.run(main(args))
--- writer.py
+++ writer.py
@@ -1,33 +1,57 @@
 import asyncio
 import argparse
+import json
+
+
+async def dialogue(reader, writer):
+
+    while True:
+        message = input('Ваше сообщение: ')
+        writer.write(f'{message}\n\n'.encode())
+        await writer.drain()
+        await reader.readline()
+
+
+async def login(reader, writer, hash):
+
+    await reader.readline()
+
+    if hash:
+
+        writer.write(f'{hash}\n'.encode())
+        await writer.drain()
+        await reader.readline()
+
+    else:
+
+        writer.write('\n'.encode())
+        await writer.drain()
+        await reader.readline()
+        username = input('Введите ник: ')
+        writer.write(f'{username}\n\n'.encode())
+        await writer.drain()
+        response_with_hash = await reader.readline()
+        hash = json.loads(response_with_hash.decode())['account_hash']
+        print(f'Ваш хеш: {hash}')
 
 
 async def main(args):
 
-    host = args.host[0]
-    port = int(args.port[0])
+    host = args.host
+    port = args.port
+    hash = args.hash
 
     reader, writer = await asyncio.open_connection(host=host, port=port)
-    await reader.readline()
-    writer.write('\n'.encode())
-    await writer.drain()
-    await reader.readline()
-    username = input('Введите ник: ')
-    writer.write(f'{username}\n'.encode())
-    await writer.drain()
-    await reader.readline()
 
-    while True:
-        message = input('Ваше сообщение: ')
-        writer.write(f'{message}\n'.encode())
-        await writer.drain()
-        await reader.readline()
+    await login(reader, writer, hash)
+    await dialogue(reader, writer)
 
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('--host', action='append', help='указать хост')
-    parser.add_argument('--port', action='append', help='указать порт')
+    parser.add_argument('--host', help='указать хост')
+    parser.add_argument('--port', type=int, help='указать порт')
+    parser.add_argument('--hash', help='указать хеш')
     args = parser.parse_args()
 
     asyncio.run(main(args))
